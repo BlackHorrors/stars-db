a404f05e803820bfb3f6e9dcac92cf995583ceb6
BlackHorrors <romandaviduc@gmail.com>
2020-02-27 14:01:17 +0200

    make error-indicator.js and fixed loading

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './spinner.css'
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-sdj1ht2ska">
+            <div className="ldio-cut5ywauhdt">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-class SwapiService {
-    _apiBase = "https://swapi.co/api";
-    async getResource(url){
-        const res = await fetch(` ${this._apiBase}${url}`);
-        if (!res.ok){
-            throw Error(`Could not fetch ${url}`+
-            `,received ${res.status}`)
-        }
-        return await  res.json();
-    }
- async getAllPeople() {
-        const  res = await  this.getResource(`/people/`);
-        return res.results;
- }
-    async getAllPlanets() {
-        const  res = await  this.getResource(`/planets/`);
-        return res.results;
-    }
-    async getAllStarships() {
-        const  res = await  this.getResource(`/starships /`);
-        return  res.results
-    }
-    getAllStarships (id) {
-        return this.getResource(`/starships/${id}`);
-    }
-
-    getPerson(id) {
-        return this.getResource(`/people/${id}/`);
-    }
-
-    getPlanet(id) {
-        return  this.getResource(`/planet/${id}`);
-
-    }
-
-}
-const swapi = new SwapiService();
-swapi.getPerson(3).then((person) => {
-    console.log(person)
-  });
-
+ReactDOM.render(<App />,
+    document.getElementById('root'));
